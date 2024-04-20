b6321796f136b8078551aad0fac525393fda5f77
sarahalab <susuhalabi22@gmail.com>
2024-04-20 15:59:29 -0400

    Initial commit

---
+++ app/src/main/java/com/cis436_project4/project4/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cis436_project4.project4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
